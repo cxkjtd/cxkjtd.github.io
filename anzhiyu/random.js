42272d9a95633faec5f1603c4e41990d29628ec9
cxkjtd <3269669967@qq.com>
2024-06-23 22:35:34 +0800

    Site updated: 2024-06-23 22:35:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/19/hello-world/"];function toRandomPost(){
+var posts=["2024/03/19/hello-world/","2024/06/22/组织学与胚胎学/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
